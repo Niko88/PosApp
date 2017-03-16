197fc1f0c4ea85962cfd66c2d5f8c0542d98012d
Nicholas <nikolas.e@me.com>
2017-03-16 14:48:45 +0000

    first UI draft

--- app/src/main/java/com/example/nicholasesposito/posapp/activities/MainActivity.java
+++ app/src/main/java/com/example/nicholasesposito/posapp/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.nicholasesposito.posapp;
+package com.example.nicholasesposito.posapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.nicholasesposito.posapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
